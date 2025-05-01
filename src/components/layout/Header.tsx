4a0f32470d9957f8d8bffe544ed03dbcc72b7690
Petrus Tlhomedi <tlhomedipetrus@gmail.com>
2025-05-01 17:42:05 +0200

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
             <a href="https://www.instagram.com/glam_gown_attire" target="_blank" rel="noreferrer" className="hover:text-gold transition-colors">
               <Instagram size={16} />
             </a>
-            <a href="https://www.tiktok.com/@glamgowns" target="_blank" rel="noreferrer" className="hover:text-gold transition-colors">
+            <a href="https://www.tiktok.com/@gradgowns" target="_blank" rel="noreferrer" className="hover:text-gold transition-colors">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M9 12a4 4 0 1 0 4 4V4a5 5 0 0 0 5 5" />
               </svg>
